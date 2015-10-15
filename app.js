5f77fe5740dbaab057e99fcd30342bccf8ec5efa
Bridgette Depay <BDep@Bridgette-Depays-MacBook-Pro.local>
2015-10-15 16:52:11 -0400

    set event listeners to each class of fruit, app.js

--- app.js
+++ app.js
@@ -15,17 +15,34 @@ window.onload = function() {
       }, 1000)
     });
   }
-
   startTimer();
 
 //Bugs: when timer goes off, numbers can go negative (tried using clearInterval or setTimeout) & when I click start button twice, time decreases by 2s
 
 
-//attaches images to class groups
-  var renderBoard = 
+//attaches images to class groups once clicked
+  var renderCards = function() {
+
+    $('.flip-card.t').on('click', function() {
+      $('.flip-card.t').append('<img src="/Users/BDep/GA-WDI/game-project/images/tomato.png" alt="Tomato"></img>')
+    }) ;
+
+
+    $('.flip-card.a').on('click', function() {
+      $('.flip-card.a').append('<img src="/Users/BDep/GA-WDI/game-project/images/apricot.png" alt="Apricot"></img>')
+    }) ;
+
+
+    $('.flip-card.e').on('click', function() {
+      $('.flip-card.e').append('<img src="/Users/BDep/GA-WDI/game-project/images/eggplant.png" alt="Eggplant"></img>')
+    }) ;
+
+  }
+ renderCards();
+//Bugs: when I click a card, every card of same type is revealed (for loop?)
 
 
 
 
 
-};
+  }
