2310cd0ecadcb93c5c20f492ad64ec0769c2d57c
Bridgette Depay <BDep@Bridgette-Depays-MacBook-Pro.local>
2015-10-14 23:02:53 -0400

    began to fill in app.js, index.html, style.css & created images folder for flip game

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+//first memory game
+
+//make board
+  // var board = [’t’, ‘a’, ‘c’,
+  //              ‘c’, ’t’, ‘a’,
+  //              ‘a’, ’t’, ‘c’];
+  //
+  // var renderBoard = function(){
+  //   for(i = 0; i < board.length; i++){
+  //     var li = $('li')
+  //
+  //     var div = document.createElement
+  //   }
+  // };
+  //
+  // renderBoard();
+
+
+// trying to figure out how to call all element (of same type) using jQuery
